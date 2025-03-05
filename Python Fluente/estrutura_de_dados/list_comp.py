d79b7c8acaa243f60cf8ddab8ff8f1e5edd24f11
Diego Neves <pdcassiano@gmail.com>
2025-03-05 13:47:50 -0300

    list comp

---
+++ Python Fluente/estrutura_de_dados/list_comp.py
@@ -0,0 +1,37 @@
+from timed import get_time
+nums = range(1, 10)
+
+@get_time
+def test1():
+    dobros = []
+    for num in nums:
+        dobros.append(num * 2)
+
+
+    # print(dobros)
+
+@get_time
+def test2():
+    dobros2 = [num * 2 for num in nums]
+
+    # print(dobros2)
+
+
+# test1()
+# test2()
+
+
+
+quadrados1 = (num * num for num in nums)
+quadrados2 = [num * num for num in nums if num % 2 == 0]
+
+print(quadrados1)
+print(quadrados2)
+
+naipes = ["Espadas", "Paus", "Copas", "Ouros"]
+valores = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+cartas = [f"{valor} de {naipe}" for valor in valores 
+                                for naipe in naipes]
+
+print(cartas)
---
+++ Util/timed.py
@@ -0,0 +1,22 @@
+import time
+
+def get_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__.upper()} EXECUTADO EM: {end - start:.6f} segundos")
+        return result
+    return wrapper
+
+if __name__ == "__main__":
+
+    @get_time
+    def test():
+        print("Teste")
+        print("Teste")
+        print("Teste")
+        print("Teste")
+
+
+    test()
