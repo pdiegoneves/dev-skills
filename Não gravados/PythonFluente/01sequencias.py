9a13a79089afda8d7e046e13c02a8e4b28b885b4
Diego Neves <pdcassiano@gmail.com>
2025-05-05 17:56:08 -0300

    desempacotamento

--- Não gravados/PythonFluente/01sequencias.py
+++ Não gravados/PythonFluente/01sequencias.py
@@ -19,3 +19,40 @@ print("IMUTAVEL")
 print("tuple", issubclass(tuple, abc.Sequence))
 print("str", issubclass(str, abc.Sequence))
 print("bytes", issubclass(bytes, abc.Sequence))
+
+
+print("-----")
+print("tuple", issubclass(tuple, abc.MutableSequence))
+print("array", issubclass(array.array, abc.Sequence))
+
+
+pessoas = [
+    ("joao", 38),
+    ("maria", 35),
+    ("pedro", 40),
+]
+
+
+array_numeros = array.array("i", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+deque_numeros = collections.deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+print(pessoas)
+print(array_numeros)
+print(deque_numeros)
+
+
+## Multabilidade
+
+frutas = ["pera", "laranja", "banana", "maca"]
+
+frutas[0] = "abacaxi"
+
+print(frutas)
+
+
+vogais = "aelou"
+print(vogais[2])
+
+vogais[2] = "i"  # TypeError: 'str' object does not support item assignment
+
+print(vogais)
--- Não gravados/PythonFluente/02desempacotamento.py
+++ Não gravados/PythonFluente/02desempacotamento.py
@@ -1,32 +1,77 @@
-list = ["primeiro", "segundo", "terceiro"]
+# list = ["primeiro", "segundo", "terceiro"]
 
 
-def funcao(a, b, c):
-    print(a, b, c)
+# def funcao(a, b, c, *d):
+#     print("Argumento 1:", a)
+#     print("Argumento 2:", b)
+#     print("Argumento 3:", c)
+#     print("demais argumentos:", d)
 
 
-funcao(*list)
+# funcao(*list)
 
 
-list2 = {
-    "primeiro": "Pera",
-    "segundo": "Laranja",
-    "terceiro": "Banana",
-    "quarto": "Maça",
-}
+# def desenpacotamento(*args):
+#     for index, valor in enumerate(args):
+#         print(f"Argumento {index + 1}: {valor}")
+
+
+# lista = ["primeiro", "segundo", "terceiro", "quarto", "quinto", "sexto"]
+# desenpacotamento(*lista)
+
+# list2 = {
+#     "primeiro": "Pera",
+#     "segundo": "Laranja",
+#     "terceiro": "Banana",
+#     "quarto": "Maça",
+#     "quinto": "Uva",
+# }
 
 
 # def funcao2(primeiro=None, segundo=None, terceiro=None):
-def funcao2(primeiro=None, segundo=None, terceiro=None, **kwargs):
-    print(primeiro, segundo, terceiro)
+# def funcao2(primeiro=None, segundo=None, terceiro=None, **kwargs):
+#     print(primeiro, segundo, terceiro, kwargs)
 
 
-funcao2(**list2)
+# funcao2(**list2)
 
-# outros usos
+# # outros usos
 
-a, b, c = range(3)
-print(a, b, c)
+# a, b, c = range(3)
+# print(a, b, c)
 
-a, b, c, *rest = range(50)
-print(a, b, c, rest)
+# a, b, c, *rest = range(50)
+# print(a, b, c, rest)
+
+
+# --------
+# lista = [
+#     "colher",
+#     "garfo",
+#     "faca",
+#     "concha",
+#     "espátula",
+#     "abridor",
+# ]
+
+# colheres, garfos, facas, *demais_utensilios = lista
+
+# print("Colheres:", colheres)
+# print("Garfos:", garfos)
+# print("Facas:", facas)
+# print("Demais utensílios:", demais_utensilios)
+
+
+def montar_macarronada(massa, molho, **ingredientes):
+    print("Massa:", massa)
+    print("Molho:", molho)
+    print(ingredientes)
+
+
+pedido = {
+    "massa": "espaguete",
+    "molho": "bolonhesa",
+    "ingredientes": ["queijo", "azeitona", "cebola"],
+}
+
+montar_macarronada(**pedido)
